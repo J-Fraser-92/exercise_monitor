665a590ad8a408ada3d05c37b3d9662ea746ef99
James Fraser <jamesfraser@jamess-mbp.home>
2016-11-11 21:28:00 +0000

    Update for date fix and tabbing

--- jobs/weights.rb
+++ jobs/weights.rb
@@ -1,43 +1,44 @@
 require 'date'
 require 'yaml'
 
-d = {
-	"bicep_curls" => {
-		"01/11/16" => {
-			:sets => 4,
-			:reps => 10, 
-			:weight => 15
-		},
-		"06/11/16" => {
-			:sets => 4,
-			:reps => 10, 
-			:weight => 20
-		}
-	}
-}
-
-File.open('jobs/data/data.yml', 'w') {|f| f.write d.to_yaml }
-
 SCHEDULER.every '1m', :first_in => 0 do
 	data = YAML::load_file('jobs/data/data.yml') 
-	weights = data['weights']
-
+	bicep_curls = data['bicep_curls']
 	points = []
-	x = Date.today.to_time.to_i 
+	x = Date.today
 	for day in (30).downto(0)
-
-		offset = day * (60 * 60 * 24)
-		unix = (x-offset)
-		days_offset = DateTime.strptime(unix.to_s, '%s')
-		date_index = days_offset.strftime("%d/%m/%y")
-		date_key = DateTime.strptime(date_index.to_s, '%s')
-		
-		
-		points << { :x => unix, :y => rand(50) }
+		day_index = x - day
+		if bicep_curls.key?(day_index)
+			details = bicep_curls[day_index]
+			value = details[:sets].to_i * details[:reps].to_i * details[:weight].to_i
+			points << { :x => day_index.to_time.to_i, :y => value }
+		else
+			points << { :x => day_index.to_time.to_i, :y => 0 }
+		end
 		
 	end
 
-	puts points
-
   	send_event('bicep_curls', points: points)
+end
+
+SCHEDULER.every '1m', :first_in => 0 do
+	data = YAML::load_file('jobs/data/data.yml') 
+	tabs = data['tabs']
+	points = []
+	x = Date.today
+	while x.wday != 4
+		x -= 1
+	end
+	for week in (10).downto(0)
+		day_index = x - (7 * week)
+		if tabs.key?(day_index)
+			details = tabs[day_index]
+			value = details[:weight].to_i
+			points << { :x => day_index.to_time.to_i, :y => value }
+		else
+			points << { :x => day_index.to_time.to_i, :y => 0 }
+		end
+	end
+
+  	send_event('tabs', points: points)
 end
